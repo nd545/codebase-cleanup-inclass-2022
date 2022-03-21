7eb170f0aedbc267e496f4e91191f32901a4ec03
nd545 <nd545@georgetown.edu>
2022-03-21 18:18:44 -0400

    new changes
    push changes

--- app/game.py
+++ app/game.py
@@ -10,7 +10,8 @@ from random import choice
 
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+valid_selections = ["rock","paper","scissors"]
+if u not in valid_selections:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +19,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_selections)
 print("COMPUTER CHOICE:", c)
 
 #
