ad2f6455c8960756729f3e63d1c60e4beb24cb9d
nd545 <97419852+nd545@users.noreply.github.com>
2022-03-28 18:00:59 -0400

    Simpler (#4)
    * readme and requirements files
    
    * changes

--- app/crypto.py
+++ app/crypto.py
@@ -7,6 +7,7 @@ import os
 import json
 from dotenv import load_dotenv
 import requests
+from app.utils import to_usd
 
 load_dotenv()
 
---
+++ app/utils.py
@@ -0,0 +1,9 @@
+def to_usd(my_price):
+    """
+    This is a docstring. It tells us what this function is about.
+    What its responsibilities are
+    What the parameters are 
+    What this function will return
+    Example of invoking the function
+    """
+    return '${:,.2f}'.format(my_price)
